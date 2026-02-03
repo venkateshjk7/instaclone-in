781fc0e7b2c86ebd236afd9aaa33b6ff7ce061eb
venkateshjk7 <venkatesh2005jk@gmail.com>
2026-02-03 21:37:37 +0530

    Stories.jsx

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -10,7 +10,7 @@ function Stories() {
   let tot = 0;
 
   useEffect(()=>{
-    fetch('http://localhost:3000/story')
+    fetch('https://json-db-pey8.onrender.com/story')
     .then(data => data.json())
     .then(data => setStories(data))
     .catch(err => console.log(err))
@@ -39,5 +39,6 @@ function Stories() {
     </div>
   )
 }
+
 
 export default Stories
