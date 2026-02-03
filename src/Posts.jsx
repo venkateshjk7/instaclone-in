14bc8deece46e56bef66b1d5fed4ebfe05ce75e1
venkateshjk7 <venkatesh2005jk@gmail.com>
2026-02-03 20:14:34 +0530

    Add files via upload

---
+++ src/Posts.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useState,useEffect } from 'react' 
+
+function Posts() {
+    
+    const [posts,setPosts] = useState([]);
+    
+    useEffect(()=>{
+        fetch("http://localhost:3000/posts").
+        then((data)=>data.json()).
+        then((data)=>setPosts(data)).
+        catch((err)=>console.log(err))
+    },[]);
+  
+    return (
+    <div className="d-flex justify-content-center">
+        {posts.length>0 ? (
+            <div>
+                {posts.map((post) => (
+                    <div className="my-3" key={post.id}>
+                        <div className="d-flex">
+                            <img className="dp rounded-circle" src={post.user.profile_pic} alt="Profile pic" />
+                            <h5>{post.user.username}</h5>
+                        </div>
+                        <img className="image" src={post.image} alt="post" />
+                        <div>
+                            <i className="bi bi-heart"></i>
+                            <i className="bi bi-chat"></i>
+                            <i className="bi bi-send"></i>
+                        </div>
+                        <div>
+                            <b>{post.likes} Likes</b>
+                        </div>
+                        <p>{post.caption}</p>
+                    </div>
+                ))}
+            </div>
+        ):(
+            <div>
+                Loading Posts
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default Posts
---
+++ src/Stories.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+function Stories() {
+
+  const [Stories,setStories] = useState([]);
+
+  const navigate = useNavigate();
+
+  let tot = 0;
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/story')
+    .then(data => data.json())
+    .then(data => setStories(data))
+    .catch(err => console.log(err))
+  },[]);
+
+  return (
+    <div className="story d-flex">
+        <div className="d-none">
+            {tot= Stories.length}
+        </div>
+      
+        {Stories.length>0 ? (
+          Stories.map((story)=>(
+            <div key={story.id} className="mx-1" onClick={() => {navigate(`/story/${story.id}/${tot}`)}}>
+              <div className="gradient-border">
+                <img src={story.user.profile_pic} alt="dp" className="story-dp rounded-circle " />
+              </div>
+              
+              <p className="text-truncate" style={{width:"50px"}}>{story.user.username}</p>
+            </div>
+          )
+          )
+        ):(
+          <p>Loading</p>
+        )}
+    </div>
+  )
+}
+
+export default Stories
