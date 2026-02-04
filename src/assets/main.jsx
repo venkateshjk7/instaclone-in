8238b79a55e912fe221af4a80f56603914bf8457
venkateshjk7 <venkatesh2005jk@gmail.com>
2026-02-04 20:51:28 +0530

    updated

--- src/assets/main.jsx
+++ src/assets/main.jsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ViewStory from './ViewStory.jsx'
